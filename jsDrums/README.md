acd30c3dd4c8ee19c39f10593b8674fb980ee045
dvbridges <david-bridges@hotmail.co.uk>
2019-06-18 23:15:54 +0100

    Adds drum machine written in HTML+CSS/JS

---
+++ jsDrums/README.md
@@ -0,0 +1,3 @@
+## A drum machine written in HTML+CSS and JS
+
+Drum sound files were taken from the first exercise from Wes Bos's JavaScript30 program:  https://github.com/wesbos/JavaScript30
---
+++ jsDrums/scripts/script.js
@@ -0,0 +1,37 @@
+var btns = document.querySelectorAll('button');
+var btnList = Array.from(btns)
+var sounds = ['resources/sounds/boom.wav',
+							'resources/sounds/kick.wav',
+							'resources/sounds/snare.wav',
+							'resources/sounds/tom.wav',
+							'resources/sounds/clap.wav']
+
+const sound = new Audio();
+
+var playSound = (file) => 
+	{sound.src=file;
+	 sound.play();} 
+
+for (let i=0; i<btnList.length; i++)
+{
+  btnList[i].addEventListener('mousedown', (e) => {
+		e.target.style.backgroundColor = 'limegreen';
+		e.target.style.borderColor = 'rebeccapurple';
+		e.target.style.borderWidth = '4px';
+		e.target.style.width = '110px';
+		e.target.style.height = '110px';
+		e.target.style.fontSize = '15px';
+		e.target.style.margin = '5px';
+		playSound(sounds[i]);	  
+	})
+
+  btnList[i].addEventListener('mouseup', (e) => {
+		e.target.style.backgroundColor='rebeccapurple';
+		e.target.style.borderColor = 'limegreen';
+	  e.target.style.borderWidth = '2px';
+		e.target.style.width = '100px';
+		e.target.style.height = '100px';
+		e.target.style.fontSize = '10px';
+		e.target.style.margin = '10px';
+	})
+}
