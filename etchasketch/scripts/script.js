7d14c12f8e73e1765478c51b56dec2ad23f34bd9
dvbridges <david-bridges@hotmail.co.uk>
2019-06-28 23:30:50 +0100

    Finalised etchasketch

--- etchasketch/scripts/script.js
+++ etchasketch/scripts/script.js
@@ -1,36 +1,98 @@
 var divs = document.getElementsByClassName("container")
+var colorType = () => {return 'black'};
+const GRIDSIZE = 500;
 
+// Add button event for setting cell color attribute
+var button = document.getElementById("colorButton");
+button.addEventListener('click', (event) => {
+	if (event.target.textContent === 'Random') {
+		button.textContent = 'Black';
+		colorType = () => {return 'black'};
+	} else {
+		button.textContent = 'Random';
+		colorType = randomColor;
+	}
+})
 
-var setGrid = function(row) 
+/**
+ * Summary. Sets color of cell.
+ *
+ * @return {string} Random hex color
+ */
+var randomColor = function() {
+	return "#"+((1<<24)*Math.random()|0).toString(16)
+}
+
+/**
+ * Add event lister for reset button
+ * Resets grid
+ */
+var reset = document.getElementById('resetButton')
+reset.addEventListener('click', () => {
+	var size = prompt("Enter grid size: ");
+	size = size > 0 ? size : 16
+	clearGrid();
+	setGrid(size);
+})
+
+/**
+ * Clears grid
+ */
+var clearGrid = function() {
+  while (divs[0].firstChild) {
+		divs[0].removeChild(divs[0].firstChild);
+	}
+}
+
+/**
+ * Summary. Creates single cell in grid
+ *
+ * @param {Object} The div row container
+ * @param {Number} The size if the cell in pixels
+ * @return {Object} The row with cells
+ */
+var setElements = function(row, size) 
 {
 	col = document.createElement("div");
-	col.style.borderColor = 'black';
-	col.style.height="30px";
-	col.style.width="30px";
+	col.style.borderColor = 'rgba(0, 0, 0, 0.05)';
+	col.style.height=`${size}px`;
+	col.style.width=`${size}px`;
 	col.style.borderStyle = "solid";
 	col.style.borderWidth="1px";
-	col = setEvent(col)
+	col = setElementColor(col)
 	row.appendChild(col);
 	return row;
 }
 
-var setEvent = function(cell) {
+/**
+ * Summary. Sets the cell color on move event
+ *
+ * @param {Object} The div cell
+ */
+var setElementColor = function(cell) {
 	cell.addEventListener('mousemove', (event) =>{
-	  event.target.style.backgroundColor = 'black';
+	  event.target.style.backgroundColor = colorType();
 	})
 	return cell;
 }
 
-
-for (let i = 0; i < 16; i++) 
-	{
-		row = document.createElement("div");
-		for (let i = 0; i < 16; i++) 
-			{
-        row = setGrid(row)
-			}
-		divs[0].appendChild(row);
+/**
+ * Summary. Sets grid
+ *
+ * @param {Number} The number of grid cells as sqrt
+ */
+var setGrid = function (size) {
+	var cellSize = parseInt(GRIDSIZE / size); 
+	for (let i = 0; i < size; i++) 
+		{
+			row = document.createElement("div");
+			for (let j = 0; j < size; j++) 
+				{
+					row = setElements(row, cellSize)
+				}
+			divs[0].appendChild(row);
+		}
 	}
 
-
+setGrid(16);
 
