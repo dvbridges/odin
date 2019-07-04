c302b694b15bf5b2e3ff3e14e327781399a1deda
dvbridges <david-bridges@hotmail.co.uk>
2019-07-04 23:40:44 +0100

    Add math functions to calculator

--- jsCalc/scripts/script.js
+++ jsCalc/scripts/script.js
@@ -3,11 +3,66 @@
 var btnPanel = document.getElementsByClassName('number-panel')
 var opPanel = document.getElementsByClassName('operation-panel')
 var digiScreen = document.getElementsByClassName('digi-screen')
-var addText = function (btn) {
-	btn.addEventListener('click', (evt) => {digiScreen[0].firstElementChild.value += evt.target.value})
-	return btn
+
+
+var multiply = function(arr) {
+	return arr.reduce((a,b) => a * b, 1);
 }
 
+var divide = function(arr) {
+	return arr.reduce((a,b) => a / b);
+}
+
+var add = function(arr) {
+	return arr.reduce((a,b) => a + b);
+}
+
+var subtract = function(arr) {
+	return arr.reduce((a,b) => a - b);
+}
+
+var getOperations = function () {
+  var ops = ['*', '/', '+', '-']
+	var funcs = [multiply, divide, add, subtract]
+	return ops.reduce((o, k, i) => ({...o, [k]: funcs[i]}), {})
+}
+
+
+var computeVal = function () {
+	// Get full expression as string
+	let arr = digiScreen[0].firstElementChild.value;
+	// Get full expression as array
+	let fullExpr = arr.split(/(\d+)/).filter(i => i.length > 0  );
+	// Get all operators as array	
+	let ops = arr.split(/\d+/).filter(i => i.length > 0  );
+	// Get operation functions as object
+	let opFunctions = getOperations()
+	
+	// Loop through each operation found in expression
+	for (let op of ops) {
+		if (fullExpr.indexOf(op) > -1) {
+			// Index operator
+			index = fullExpr.indexOf(op);
+			// Get digits before and after operator
+			expr = [Number(fullExpr[index-1]), Number(fullExpr[index+1])]; 
+			// Make calculation and replace calculated number and operator with outcome
+			fullExpr.splice(index-1, index+2, opFunctions[op](expr));
+		}	
+	}
+
+}
+
+
+var addText = function (btn) {
+	btn.addEventListener('mousedown', (evt) => {
+		if (evt.target.value !== '=') {
+			digiScreen[0].firstElementChild.value += evt.target.value;
+		} else {
+			computeVal()
+		}
+	})
+	return btn
+}
 
 var createButton = function(n=0) {
   var btn = document.createElement("button");
@@ -39,7 +94,7 @@ var createOperationBtns = function() {
 	for (let row = 0; row < 3; row++) {
 		div = document.createElement("div");
 		for (let col = 0; col < 2; col++) {
-			div.appendChild(createButton(ops[n]));		
+			div.appendChild(addText(createButton(ops[n])));		
 			n += 1;
 		}
 	opPanel[0].appendChild(div);
